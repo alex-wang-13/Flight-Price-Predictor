ddce18ea367f2ef27d0f925b5d8fced31a7ea769
Alex Wang <iamalex11303@gmail.com>
2024-01-02 12:37:59 -0500

    added default file path for no argument given and calculated correlation matrix

--- predictor.py
+++ predictor.py
@@ -3,11 +3,24 @@ import datetime as dt
 
 from sklearn.linear_model import LinearRegression
 
+"""
+A program to predict the price column of a .csv file dataframe.
+"""
+
+DEFAULT = "training-data.csv"
 TRAIN_FP = input("Enter the file path to the training data: ")
+TRAIN_FP = TRAIN_FP.strip()
+if not TRAIN_FP:
+  TRAIN_FP = DEFAULT
 
 """
 Get the training data.
 """
-train = pd.read_csv(TRAIN_FP)
+train: pd.DataFrame = pd.read_csv(TRAIN_FP)
 train.info()
 
+"""
+Get the correlation matrix to the price column.
+"""
+corr_matrix = train.corr()['price']
+print(corr_matrix)
